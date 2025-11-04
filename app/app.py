295c5c91ae28660d72aa08b26e73d6f61901f082
SantyBJ <santiago.bejaranobe@ecci.edu.co>
2025-11-03 19:45:34 -0500

    Se modifica app para que funcione en railway

--- app/app.py
+++ app/app.py
@@ -25,6 +25,6 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
-    #port = int(os.getenv('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port, debug=False)
+    #app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
